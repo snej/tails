08b73fa3c8105105853b8919f03d935ebe9259e8
Jens Alfke <jens@couchbase.com>
2021-05-04 10:15:24 -0700

    Initial Commit

---
+++ Tails/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tails
+//
+//  Created by Jens Alfke on 5/4/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
